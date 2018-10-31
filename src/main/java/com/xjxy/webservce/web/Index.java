5384dbb610ca590f6e4590cad109aaa2bf402b0a
unknown <liwei@zzsim.com>
2018-10-31 18:28:18 +0800

    Initial commit

---
+++ src/main/java/com/xjxy/webservce/web/Index.java
@@ -0,0 +1,22 @@
+package com.xjxy.webservce.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * 首页
+ * @author liwei
+ * @date 2018/10/23 10:02
+ **/
+@Controller
+public class Index {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index.html";
+    }
+    @RequestMapping("/home/home")
+    public String home(){
+        return "home.html";
+    }
+}
