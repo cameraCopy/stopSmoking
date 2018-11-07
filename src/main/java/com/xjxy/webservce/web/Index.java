89fa4c17d07f5fcc9fb5c8d2b41a770dd58adea2
root <promise>
2018-11-07 18:31:39 +0800

    添加官网模版

--- src/main/java/com/xjxy/webservce/web/Index.java
+++ src/main/java/com/xjxy/webservce/web/Index.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class Index {
 
-    @RequestMapping("/")
+    @RequestMapping("/index")
     public String index(){
         return "index.html";
     }
